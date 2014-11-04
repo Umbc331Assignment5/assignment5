5b310c354062e838a5c78f851b8bf8e5339c8da6
Matt Henry <eldushbago@gmail.com>
2014-11-04 16:20:57 -0500

    reads data headers

--- assignment5.c
+++ assignment5.c
@@ -58,6 +58,7 @@ static const struct typemasks
 	uint8_t stop;// = 11;
 } typemask = { 0, 1, 2, 3, 7, 8, 9, 10, 11};
 
+
 int main(int argc, char ** argv)
 {
 	argv++;
@@ -73,15 +74,21 @@ int main(int argc, char ** argv)
 	if (headbuff==NULL) { printf("Memory error\n"); return -1;}
 	fp = fopen(*argv, "rb");
 	if (fp==NULL) { printf("Error opening file\n"); return -2; }
-	result = fread(headbuff, 4, 1, fp);
 
-	int theversion = headbuff->vt.version & 0x0F;
-	int thetype = (headbuff->vt.type & 0xF0) >> 4;
-	int thelength = headbuff->length;
-	int skipbit = headbuff->s & 0x1;
-
-	printf("We read version %d type %d length %d skipbit %d\n",
+	int i;
+	while((result = fread(headbuff, 4, 1, fp)) == 1)
+	{
+		int theversion = headbuff->vt.version & 0x0F;
+		int thetype = (headbuff->vt.type & 0xF0) >> 4;
+		int thelength = headbuff->length;
+		int skipbit = headbuff->s & 0x1;
+		printf("We read version %d type %d length %d skipbit %d\n",
 			theversion, thetype ,thelength ,skipbit);
+		
+		//free(headbuff);
+	}
+
+	
 	//while(c != EOF)
 	//{
 	//	c = getc(fp);
@@ -97,6 +104,41 @@ void read_header(FILE * fp)
 {
 	
 }
+
+//Assumes length is the number of the items specified
+void read_sixteenbit_i(uint8_t length)
+{
+	int numbytes = length * 2;
+}
+
+void read_thirtytwobit_i(uint8_t length)
+{
+	int numbytes = length * 4;
+}
+
+void read_sixteenbit_f(uint8_t length)
+{
+	int numbytes = length * 2;
+}
+
+void read_thirtytwobit_f(uint8_t length)
+{
+	int numbytes = length * 4;
+}
+
+void read_ascii()
+{
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
