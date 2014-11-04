22d258b63a22ac8d4b035377008cd1a85001ca4c
Matt Henry <eldushbago@gmail.com>
2014-11-04 18:29:29 -0500

    structs updated

--- assignment5.c
+++ assignment5.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+void read_sixteenbit_i(FILE * fp, uint8_t length);
+void read_thirtytwobit_i(FILE * fp, uint8_t length);
+void read_thirtytwobit_f(FILE * fp, uint8_t length);
+void read_sixtyfourbit_f(FILE * fp, uint8_t length);
+void read_ascii(FILE * fp, uint8_t length);
+void read_data(FILE * fp, uint8_t type, uint8_t length);
+
 typedef struct dummy
 {
 
@@ -16,32 +23,33 @@ typedef struct dummy
 
 typedef struct datagram_v1
 {
-	uint8_t version : 4;
-	uint8_t type : 4;
+	union versiontype vt;
 	uint8_t length;
-	uint16_t s : 1;
+	uint8_t s : 1;
 	//uint32_t data;
 } datagram_v1;
 
 typedef struct datagram_v2
 {
-	uint8_t version : 4;
-	uint8_t type : 4;
+	union versiontype vt;
 	uint8_t length;
-	uint8_t s : 1;
-	uint8_t duplicate : 1;
-	uint16_t checksum;
+	union skipdup {
+		uint8_t s;
+		uint8_t duplicate;
+	} skipdup;
+	uint8_t checksum;
 	//uint32_t data;
 } datagram_v2;
 
 typedef struct datagram_v3
 {
-	uint8_t version : 4;
-	uint8_t type : 4;
+	union versiontype vt;
 	uint8_t length;
-	uint16_t s : 1;
-	uint16_t id : 15;
-	uint16_t checksum;
+	union skip_id {
+		uint8_t s;
+		uint8_t id;
+	} sid;
+	uint8_t checksum;
 	//uint32_t data;
 } datagram_v3;
 
@@ -65,7 +73,7 @@ int main(int argc, char ** argv)
 	FILE * fp = NULL;
 	size_t result;
 	dummy d;
-	printf("Dummy size %lu\n", sizeof(d)); // should be 4
+	printf("Datagram3 size %lu\n", sizeof(datagram_v3)); // should be 4
 	
 	dummy * headbuff = malloc(4); // all versions have a header 4 bytes long
 	if (headbuff==NULL) { printf("Memory error\n"); return -1;}
@@ -75,12 +83,13 @@ int main(int argc, char ** argv)
 	int i;
 	while((result = fread(headbuff, 4, 1, fp)) == 1)
 	{
-		int theversion = headbuff->vt.version & 0x0F;
-		int thetype = (headbuff->vt.type & 0xF0) >> 4;
-		int thelength = headbuff->length;
-		int skipbit = headbuff->s & 0x1;
+		uint8_t theversion = headbuff->vt.version & 0x0F; //gets lower 4 bits
+		uint8_t thetype = (headbuff->vt.type & 0xF0) >> 4; 	//gets higher 4 bits
+		uint8_t thelength = headbuff->length;
+		uint8_t skipbit = headbuff->s & 0x1;				//takes first bit
 		printf("We read version %d type %d length %d skipbit %d\n",
-			theversion, thetype ,thelength ,skipbit);
+			(int)theversion, (int)thetype ,(int)thelength ,(int)skipbit);
+		read_data(fp, thetype, thelength);
 		
 	}
 
@@ -95,28 +104,33 @@ void read_header(FILE * fp)
 	
 }
 
-void version(uint8_t v)
+void read_data(FILE * fp, uint8_t type, uint8_t length)
 {
-	switch(v)
+	switch(type)
 	{
 		case 0: // 16 bit int
 		{
+			read_sixteenbit_i(fp,length);
 			break;
 		}
 		case 1: // 32 bit int
 		{
+			read_thirtytwobit_i(fp, length);
 			break;
 		}
 		case 2: // 32 bit float
 		{
+			read_thirtytwobit_f(fp,length);
 			break;
 		}
 		case 3: // 64 bit float
 		{
+			read_sixtyfourbit_f(fp,length);
 			break;
 		}
 		case 7: // ascii
 		{
+			read_ascii(fp,length);
 			break;
 		}
 		case 8: // junk
@@ -149,26 +163,31 @@ void version(uint8_t v)
 void read_sixteenbit_i(FILE * fp, uint8_t length)
 {
 	int numbytes = length * 2;
+	printf("Bytes: %d\n", numbytes);
 }
 
 void read_thirtytwobit_i(FILE * fp, uint8_t length)
 {
 	int numbytes = length * 4;
+	printf("Bytes: %d\n", numbytes);
 }
 
 void read_thirtytwobit_f(FILE * fp, uint8_t length)
 {
 	int numbytes = length * 4;
+	printf("Bytes: %d\n", numbytes);
 }
 
 void read_sixtyfourbit_f(FILE * fp, uint8_t length)
 {
 	int numbytes = length * 8;
+	printf("Bytes: %d\n", numbytes);
 }
 
 void read_ascii(FILE * fp, uint8_t length)
 {
-
+	int numbytes = length;
+	printf("Bytes: %d\n", numbytes);
 }
 
 
